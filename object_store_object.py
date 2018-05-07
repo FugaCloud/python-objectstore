647b9e26bcad51975e9b9c8d6c14bb40ebce40ce
thomas <thomas.timmer@cyso.com>
2018-05-07 17:24:45 +0200

    Added tests with 100%

--- object_store_object.py
+++ object_store_object.py
@@ -5,8 +5,6 @@ import boto
 import boto.s3.connection
 from boto.s3.key import Key
 
-ACCESS_KEY = '29aba3ce5c70426084844bc10e2dd86b'
-SECRET_KEY = 'b6274b5c96cc40639151ada6bac03833'
 
 class FugaContainer(object):
 
@@ -15,10 +13,17 @@ class FugaContainer(object):
         self.secret_key = secret_key
         self.container = None
 
+    def _fetch_container(self, container_name):
+        if container_name is None:
+            container = self.container
+            if container is None:
+                raise AttributeError('Container is not set')
+        else:
+            container = self.get_container(container_name)
+        return container
 
     def get_container(self, container_name):
         return self.set_container(container_name, set_object=False)
-
 
     def set_container(self, container_name, set_object=True):
         conn = boto.connect_s3(
@@ -28,32 +33,23 @@ class FugaContainer(object):
             port=443,
             calling_format=boto.s3.connection.OrdinaryCallingFormat(),
         )
+        container = conn.get_bucket(container_name)
         if set_object:
-            self.container = conn.get_bucket(container_name)
-        return self.container
+            self.container = container
+        return container
 
 
-    def upload_file(self, file, container=None):
-        if container is not None:
-            bucket = self.get_container(container)
-        elif self.container is None:
-            raise AttributeError('Container is not set')
-        else:
-            bucket = self.container
-
+    def upload_file(self, file, container_name=None):
+        container = self._fetch_container(container_name)
         with open(file, 'rb') as f:
             name = basename(f.name)
-            k = bucket.new_key(name)
+            k = container.new_key(name)
             k.set_contents_from_file(f)
-    
 
-    def get_files(self, container=None, return_binary=False, limit=None):
+    def get_files(self, container_name=None, return_binary=False, limit=None):
         files = {}
-        if container is None:
-            container = self.container
-            if container is None:
-                raise AttributeError('Container is not set')
-        
+        container = self._fetch_container(container_name)
+
         for i, key in enumerate(container.list()):
             file = key.get_contents_as_string()
             if not return_binary:
@@ -61,17 +57,12 @@ class FugaContainer(object):
             files.update({key.name: file})
 
             if limit is not None:
-                if i == limit:
+                if i == limit-1:
                     break
         return files
 
-
-    def get_file(self, filename, container=None, return_binary=False):
-        if container is None:
-            container = self.container
-            if container is None:
-                raise AttributeError('Container is not set')
-
+    def get_file(self, filename, container_name=None, return_binary=False):
+        container = self._fetch_container(container_name)
         if filename in [x.name for x in container.list()]:
             key = container.get_key(filename)
             file = key.get_contents_as_string()
@@ -80,55 +71,60 @@ class FugaContainer(object):
             return file
         raise AttributeError('File not found')
 
-
-    def delete_file(self, filename, container=None):
-        if container is None:
-            container = self.container
-            if container is None:
-                raise AttributeError('Container is not set')
-        
+    def delete_file(self, filename, container_name=None):
+        container = self._fetch_container(container_name)
         if filename in [x.name for x in container.list()]:
             k = container.get_key(filename)
             k.delete()
             return "'{}' deleted".format(filename)
 
         raise AttributeError('File not found')
-        
 
-    def save_file(self, filename, container=None):
+    def save_file(self, filename, container_name=None):
         """saves the 'filename' from the container to 'filename' on the local computer"""
-        file =  self.get_file(filename, container, return_binary=True)
+        container = self._fetch_container(container_name)
+        file = self.get_file(filename, container, return_binary=True)
         with open(filename, 'wb') as f:
             f.write(file)
-        
+
         return 'successfully saved file'
 
-        raise AttributeError('File not found')
-
+    def list_files(self, container_name=None):
+        container = self._fetch_container(container_name)
+        return [x.name for x in container.list()]
 
     def __repr__(self):
-        base_repr = "FugaContainer(access_key={}, secret_key=<secret_key>)".format(self.access_key)
+        base_repr = "FugaContainer(access_key={}, secret_key=<secret_key>)".format(
+            self.access_key)
         if self.container is None:
             return base_repr
         return "{} with container {}".format(base_repr, self.container)
-        
 
     @staticmethod
     def list_functions():
         return [x for x in dir(FugaContainer) if not x.startswith("_")]
 
-if __name__ == "__main__":
-    fuga = FugaContainer(ACCESS_KEY, SECRET_KEY)
-    fuga.set_container('flask-test-wauw')
-    print(fuga)
 
-    print(FugaContainer.list_functions())
+if __name__ == "__main__":
+
+    # ACCESS_KEY = '29aba3ce5c70426084844bc10e2dd86b'
+    # SECRET_KEY = 'b6274b5c96cc40639151ada6bac03833'
+
+    # fuga = FugaContainer(ACCESS_KEY, SECRET_KEY)
+    # fuga.set_container('flask-test-wauw')
+    # print(fuga)
+
+    # print(FugaContainer.list_functions())
 
     # print(dir(fuga))
     # fuga.upload_file('/home/thomas/Python/django-test/static/netecht.png')
-    fuga.upload_file('/home/thomas/Downloads/Microsoft Office 97 Professional (ISO).7z')
+    # fuga.upload_file('/home/thomas/Downloads/Microsoft Office 97 Professional (ISO).7z')
     # fuga.upload_file('/home/thomas/Git/blade-bootstrap/blade/run_tests.sh')
     # print(fuga.get_files())
     # print(fuga.delete_file('netecht.png'))
     # print(fuga.get_file('netecht.png'))
-    # print(fuga.save_file('netecht.sh'))
+    # print(fuga.save_file('netecht.png'))
+    # print(fuga.list_files('flask-test-wauw'))
+
+
+    pass
---
+++ tests/test_fuga_container.py
@@ -0,0 +1,243 @@
+from object_store_object import FugaContainer
+import pytest
+from unittest.mock import patch, MagicMock, Mock, mock_open
+from boto.exception import S3ResponseError
+
+
+class MockConnectionReturnValue(object):
+
+    def __init__(self, listvalue):
+        self.listvalue = listvalue
+
+    def list(self):
+        return self.listvalue
+
+    def get_key(self, *args, **kwargs):
+        return MockFile(args[0])
+
+    def new_key(self, *args, **kwargs):
+        return MockFile(args[0])
+    
+
+
+class MockFile(object):
+
+    def __init__(self, value):
+        self.name = value
+
+    def delete(self):
+        pass
+
+    def get_contents_as_string(self):
+        return bytes([0,0,0,0,0,0,0,0,0,0,0,0,0])
+
+    def set_contents_from_file(self, *args, **kwargs):
+        pass
+
+@pytest.mark.skip
+def test_forbidden_not_access_keys(mock_boto):
+    with pytest.raises(S3ResponseError):
+        FugaContainer("", "").set_container("test")
+
+
+@patch('object_store_object.boto')
+def test_set_container_sets_container(mock_boto):
+    mock_connect = Mock()
+    mock_connect.get_bucket.side_effect = lambda x: 'testing'
+    mock_boto.connect_s3.return_value = mock_connect
+    fuga = FugaContainer("", "")
+    fuga.set_container("testing")
+    assert fuga.container == 'testing'
+
+
+@patch('object_store_object.boto')
+def test_get_container_does_not_set_container(mock_boto):
+    mock_connect = Mock()
+    mock_connect.get_bucket.side_effect = lambda x: 'testing'
+    mock_boto.connect_s3.return_value = mock_connect
+    fuga = FugaContainer("", "")
+    fuga.get_container("testing")
+    assert fuga.container != 'testing'
+
+
+def test_getfile_none_container_raises_error():
+    fuga = FugaContainer("", "")
+
+    with pytest.raises(AttributeError):
+        fuga.get_file("")
+
+
+def test_getfiles_none_container_raises_error():
+    fuga = FugaContainer("", "")
+
+    with pytest.raises(AttributeError):
+        fuga.get_files()
+
+
+def test_delete_file_none_container_raises_error():
+    fuga = FugaContainer("", "")
+
+    with pytest.raises(AttributeError):
+        fuga.delete_file("")
+
+
+def test_save_file_none_container_raises_error():
+    fuga = FugaContainer("", "")
+
+    with pytest.raises(AttributeError):
+        fuga.save_file("")
+
+
+def test_upload_file_none_container_raises_error():
+    fuga = FugaContainer("", "")
+
+    with pytest.raises(AttributeError):
+        fuga.upload_file("")
+
+def test_list_files_none_container_raises_error():
+    fuga = FugaContainer("", "")
+
+    with pytest.raises(AttributeError):
+        fuga.list_files()
+
+def test_list_all_the_functions():
+
+    assert set(FugaContainer.list_functions()) == {'delete_file', 'get_container',
+                                                   'get_file', 'get_files',
+                                                   'list_functions', 'save_file', 
+                                                   'set_container', 'upload_file', 
+                                                   'list_files'}
+
+@patch('object_store_object.boto')
+def test_fuga_container_repr_function(mock_boto):
+    fuga = FugaContainer("", "")
+    assert str(fuga) == "FugaContainer(access_key="", secret_key=<secret_key>)"
+
+    mock_connect = Mock()
+    mock_connect.get_bucket.side_effect = lambda x: 'test'
+    mock_boto.connect_s3.return_value = mock_connect
+
+    fuga.set_container("test")
+    assert str(fuga) == "FugaContainer(access_key="", secret_key=<secret_key>) with container test"
+
+
+@patch('object_store_object.boto')
+def test_list_files_function(mock_boto):
+    fuga = FugaContainer("", "")
+
+    mock_connection = MockConnectionReturnValue(listvalue=[MockFile('1'), MockFile('2'), MockFile('3')])
+    mock_connect = Mock()
+    mock_connect.get_bucket.side_effect = lambda x: mock_connection
+    mock_boto.connect_s3.return_value = mock_connect
+
+    assert fuga.list_files("") == ['1', '2', '3']
+    fuga.set_container("")
+    assert fuga.list_files() == ['1', '2', '3']
+
+@patch('object_store_object.boto')
+def test_delete_file_succeeds(mock_boto):
+    fuga = FugaContainer("", "")
+
+    mock_connection = MockConnectionReturnValue(listvalue=[MockFile('1'), MockFile('2'), MockFile('3')])
+    mock_connect = Mock()
+    mock_connect.get_bucket.side_effect = lambda x: mock_connection
+    mock_boto.connect_s3.return_value = mock_connect
+    assert fuga.delete_file("1", "") == "'1' deleted"
+    fuga.set_container("")
+    assert fuga.delete_file("2") == "'2' deleted"
+
+
+@patch('object_store_object.boto')
+def test_delete_file_fails(mock_boto):
+    fuga = FugaContainer("", "")
+
+    mock_connection = MockConnectionReturnValue(listvalue=[MockFile('1'), MockFile('2'), MockFile('3')])
+    mock_connect = Mock()
+    mock_connect.get_bucket.side_effect = lambda x: mock_connection
+    mock_boto.connect_s3.return_value = mock_connect
+    with pytest.raises(AttributeError):
+        fuga.delete_file("4", "")
+    fuga.set_container("")
+    with pytest.raises(AttributeError):
+        fuga.delete_file("4")
+
+@patch('object_store_object.boto')
+def test_save_file_succeeds(mock_boto):
+    fuga = FugaContainer("", "")
+
+    mock_connection = MockConnectionReturnValue(listvalue=[MockFile('1'), MockFile('2'), MockFile('3')])
+    mock_connect = Mock()
+    mock_connect.get_bucket.side_effect = lambda x: mock_connection
+    mock_boto.connect_s3.return_value = mock_connect
+    fuga.set_container("")
+
+    with patch("builtins.open", mock_open()) as mock_file:
+        fuga.save_file("1")
+
+@patch('object_store_object.boto')
+def test_get_file(mock_boto):
+    fuga = FugaContainer("", "")
+
+    mock_connection = MockConnectionReturnValue(listvalue=[MockFile('1'), MockFile('2'), MockFile('3')])
+    mock_connect = Mock()
+    mock_connect.get_bucket.side_effect = lambda x: mock_connection
+    mock_boto.connect_s3.return_value = mock_connect
+    fuga.set_container("")
+
+    with patch("builtins.open", mock_open()) as mock_file:
+        fuga.get_file("1")
+
+
+@patch('object_store_object.boto')
+def test_get_file_doesnt_exist(mock_boto):
+    fuga = FugaContainer("", "")
+
+    mock_connection = MockConnectionReturnValue(listvalue=[MockFile('1'), MockFile('2'), MockFile('3')])
+    mock_connect = Mock()
+    mock_connect.get_bucket.side_effect = lambda x: mock_connection
+    mock_boto.connect_s3.return_value = mock_connect
+    fuga.set_container("")
+
+    with pytest.raises(AttributeError):
+        fuga.get_file("4")
+
+
+@patch('object_store_object.boto')
+@patch('object_store_object.basename')
+def test_upload_file_works(mock_basename, mock_boto):
+    fuga = FugaContainer("", "")
+
+    mock_connection = MockConnectionReturnValue(listvalue=[MockFile('1'), MockFile('2'), MockFile('3')])
+    mock_connect = Mock()
+    mock_connect.get_bucket.side_effect = lambda x: mock_connection
+    mock_boto.connect_s3.return_value = mock_connect
+    fuga.set_container("")
+
+    with patch("builtins.open", mock_open()) as mock_file:
+        fuga.upload_file("4")
+
+
+@patch('object_store_object.boto')
+def test_get_files_no_limit(mock_boto):
+    fuga = FugaContainer("", "")
+
+    mock_connection = MockConnectionReturnValue(listvalue=[MockFile('1'), MockFile('2'), MockFile('3')])
+    mock_connect = Mock()
+    mock_connect.get_bucket.side_effect = lambda x: mock_connection
+    mock_boto.connect_s3.return_value = mock_connect
+    fuga.set_container("")
+
+    fuga.get_files()
+
+@patch('object_store_object.boto')
+def test_get_files_with_limit(mock_boto):
+    fuga = FugaContainer("", "")
+
+    mock_connection = MockConnectionReturnValue(listvalue=[MockFile('1'), MockFile('2'), MockFile('3')])
+    mock_connect = Mock()
+    mock_connect.get_bucket.side_effect = lambda x: mock_connection
+    mock_boto.connect_s3.return_value = mock_connect
+    fuga.set_container("")
+
+    assert len(fuga.get_files(limit=2)) == 2
+
